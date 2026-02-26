674f9d4e0143b490d2c6fe35557b2dd9f27787c2
fsquarefurniture <fsquarefuniture@gmail.com>
2026-02-26 09:09:16 +0100

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <section className="p-10">
+      <h1 className="text-4xl font-bold mb-4">About F-Square</h1>
+      <p className="text-lg text-gray-400">
+        F-Square Furniture is a luxury modern furniture brand focused on
+        premium craftsmanship, elegance, and timeless design.
+      </p>
+    </section>
+  )
+}
