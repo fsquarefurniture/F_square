6bba7bf85013f8c2234e1e621f18d97dda04017a
fsquarefurniture <fsquarefuniture@gmail.com>
2026-02-26 10:43:54 +0100

    Create GalleryItem.js

---
+++ app/components/GalleryItem.js
@@ -0,0 +1,7 @@
+export default function GalleryItem({ title }) {
+  return (
+    <div className="border border-gray-800 p-6 rounded-lg">
+      <h3 className="text-xl font-semibold">{title}</h3>
+    </div>
+  )
+}
