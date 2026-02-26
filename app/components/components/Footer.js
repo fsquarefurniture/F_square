d165e0d15dc31cd09716381df5f9ed1a238a44b3
fsquarefurniture <fsquarefuniture@gmail.com>
2026-02-26 09:35:15 +0100

    Create Footer.js

---
+++ app/components/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-6 border-t border-gray-800 mt-10">
+      <p className="text-gray-500">
+        © {new Date().getFullYear()} F-Square Furniture. All rights reserved.
+      </p>
+    </footer>
+  )
+}
